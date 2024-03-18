d4e67d9b48f66c1a7f35ead8687d7d8864981d77
MD Nazmul Hasan <mdnazmulhasanniloy323@gmail.com>
2024-03-19 01:39:57 +0600

    [Done] user get, delete, update, bloked Api is creating

--- generateFolder.ts
+++ generateFolder.ts
@@ -1,4 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const fs = require('fs');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path');
 
 // Function to create the folder and files
