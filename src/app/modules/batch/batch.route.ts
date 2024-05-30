420661c317e7e5092b72671ba20aeca26f830c1e
MD Nazmul Hasan <mdnazmulhasanniloy323@gmail.com>
2024-05-31 02:06:09 +0600

    some chengs

--- src/app/modules/batch/batch.route.ts
+++ src/app/modules/batch/batch.route.ts
@@ -2,20 +2,23 @@ import { Router } from 'express';
 import { BatchController } from './batch.controller';
 import validateRequest from '../../middlewares/validateRequest';
 import { batchValidation } from './batch.validation';
+import { verifyToken } from '../../middlewares/verifyToken';
 
 const router = Router();
 
 router.post(
   '/create-batch',
+  verifyToken,
   validateRequest(batchValidation.createBatchZodSchema),
   BatchController.createBatch,
 );
 router.patch(
   '/:id',
+  verifyToken,
   validateRequest(batchValidation.updateBatchZodSchema),
   BatchController.updateBatch,
 );
-router.delete('/:id', BatchController.deleteBatch);
+router.delete('/:id', verifyToken, BatchController.deleteBatch);
 router.get('/:id', BatchController.getBatchById);
 router.get('/', BatchController.getAllBatch);
 
