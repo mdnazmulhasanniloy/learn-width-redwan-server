3a9b17052330b7d0cc6e9cf761378768efa4e5bb
mdnazmulhasanniloy <mdnazmulhasanniloy323@gmail.com>
2024-07-31 17:54:43 +0600

    generate folder some changes

--- generateFolder.ts
+++ generateFolder.ts
@@ -1,7 +1,16 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
+// import  fs from "fs";
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const fs = require('fs');
 // eslint-disable-next-line @typescript-eslint/no-var-requires
+// import  path from "path";
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path');
+
+function capitalizeFirstLetter(text) {
+  const [first = '', ...rest] = text;
+  return [first.toUpperCase(), ...rest].join('');
+}
 
 // Function to create the folder and files
 function createFolderAndFiles(parentFolderPath, folderName) {
@@ -21,7 +30,58 @@ function createFolderAndFiles(parentFolderPath, folderName) {
 
   files.forEach(file => {
     const filePath = path.join(folderPath, file);
-    fs.writeFileSync(filePath, 'utf8');
+    let content = '';
+
+    if (file === `${folderName}.service.ts`) {
+      content = `const create${capitalizeFirstLetter(folderName)} = async () => {};
+const getAll${capitalizeFirstLetter(folderName)} = async () => {};
+const get${capitalizeFirstLetter(folderName)}ById = async () => {};
+const update${capitalizeFirstLetter(folderName)} = async () => {};
+const delete${capitalizeFirstLetter(folderName)} = async () => {};
+
+export const ${folderName}Service = {
+  create${capitalizeFirstLetter(folderName)},
+  getAll${capitalizeFirstLetter(folderName)},
+  get${capitalizeFirstLetter(folderName)}ById,
+  update${capitalizeFirstLetter(folderName)},
+  delete${capitalizeFirstLetter(folderName)},
+};`;
+    } else if (file === `${folderName}.controller.ts`) {
+      content = `import { Request, Response } from 'express';
+import catchAsync from '../../utils/catchAsync';
+
+const create${capitalizeFirstLetter(folderName)} = catchAsync(async (req: Request, res: Response) => {});
+const getAll${capitalizeFirstLetter(folderName)} = catchAsync(async (req: Request, res: Response) => {});
+const get${capitalizeFirstLetter(folderName)}ById = catchAsync(async (req: Request, res: Response) => {});
+const update${capitalizeFirstLetter(folderName)} = catchAsync(async (req: Request, res: Response) => {});
+const delete${capitalizeFirstLetter(folderName)} = catchAsync(async (req: Request, res: Response) => {});
+
+export const ${folderName}Controller = {
+  create${capitalizeFirstLetter(folderName)},
+  getAll${capitalizeFirstLetter(folderName)},
+  get${capitalizeFirstLetter(folderName)}ById,
+  update${capitalizeFirstLetter(folderName)},
+  delete${capitalizeFirstLetter(folderName)},
+};`;
+    } else if (file === `${folderName}.route.ts`) {
+      content = `import { Router } from 'express';
+import { ${folderName}Controller } from './${folderName}.controller';
+
+const router = Router();
+
+router.post('/create-${folderName}', ${folderName}Controller.create${capitalizeFirstLetter(folderName)});
+
+router.patch('/update/:id', ${folderName}Controller.update${capitalizeFirstLetter(folderName)});
+
+router.delete('/:id', ${folderName}Controller.delete${capitalizeFirstLetter(folderName)});
+
+router.get('/:id', ${folderName}Controller.get${capitalizeFirstLetter(folderName)}ById);
+router.get('/', ${folderName}Controller.getAll${capitalizeFirstLetter(folderName)});
+
+export const ${folderName}Routes = router;`;
+    }
+
+    fs.writeFileSync(filePath, content, 'utf8');
   });
 
   console.log(`Folder "${folderName}" and files created successfully.`);
